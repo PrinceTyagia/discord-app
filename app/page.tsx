b9c0fd8107dc550653a69c8cc6b14dfdfb0fe518
PrinceTyagia <veertyagia@gmail.com>
2023-08-26 13:52:23 +0530

    Add shadcn-ui components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-
-
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
-  <h1>hello</h1>
-  )
+    <div>
+      <p className=" text-3xl  font-bold text-indigo-500">
+        Hello Discord clone
+      </p>
+      <Button>click me</Button>
+    </div>
+  );
 }
